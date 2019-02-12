2ad1da47e4078137e72f880f4235b9ea98d677d6
Thomas Colligan <thomas.colligan@outlook.com>
2019-02-11 19:40:27 -0700

    Adding image augmentation

--- fully_conv.py
+++ fully_conv.py
@@ -12,8 +12,6 @@ from glob import glob
 from data_utils_polygons import generate_class_mask, normalize_image
 from tensorflow.keras.layers import (Conv2D, Input, MaxPooling2D, Conv2DTranspose, 
 Concatenate, Dropout, UpSampling2D)
-import keras
-print(keras.__version__)
 from tensorflow.keras.models import Model
 
 NO_DATA = 3
@@ -21,49 +19,49 @@ concatenate=Concatenate
 def unet(input_size = (1080, 1920, 3)):
     inputs = Input(input_size)
     reshaped = tf.keras.layers.Cropping2D(cropping=((0, 8), (0, 0)))(inputs)
-    conv1 = Conv2D(8, 3, activation = 'relu', padding = 'same', kernel_initializer =
+    conv1 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer =
             'he_normal')(reshaped)
-    conv1 = Conv2D(8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    conv1 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    conv2 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     conv4 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    conv5 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
-    up6 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    up6 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
     merge6 = concatenate()([drop4,up6])
-    conv6 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
-    up7 = Conv2D(16, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    up7 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
     merge7 = concatenate()([conv3,up7])
-    conv7 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
-    up8 = Conv2D(8, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    up8 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
     merge8 = concatenate()([conv2,up8])
-    conv8 = Conv2D(8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
-    up9 = Conv2D(4, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    up9 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
     merge9 = concatenate()([conv1,up9])
-    conv9 = Conv2D(4, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(4, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = Conv2D(filters=2, kernel_size=1, activation = 'sigmoid')(conv9)
     out = tf.keras.layers.ZeroPadding2D(((0, 8), (0, 0)))(conv10)
     model = Model(inputs=inputs, outputs=out)
-    model.summary()
+    #model.summary()
 
     return model 
 
@@ -81,29 +79,29 @@ def custom_objective(y_true, y_pred):
 def fcnn_functional(image_shape, n_classes):
     x = Input(image_shape)
 
-    c1 = Conv2D(filters=16, kernel_size=(3,3), activation='relu', padding='same')(x)
+    c1 = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
     mp1 = MaxPooling2D(pool_size=2, strides=(2, 2))(c1)
 
-    c2 = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(mp1)
+    c2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(mp1)
     mp2 = MaxPooling2D(pool_size=2, strides=(2, 2))(c2)
 
-    c3 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(mp2)
+    c3 = Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same')(mp2)
     mp3 = MaxPooling2D(pool_size=2, strides=(2, 2))(c3)
     
-    last_conv = Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same')(mp3)
-    out_shp = c2.shape
-    u1 = Conv2DTranspose(filters=128, kernel_size=(3, 3), strides=(2, 2))(last_conv)
+    last_conv = Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='same')(mp3)
+
+    u1 = Conv2DTranspose(filters=256, kernel_size=(3, 3), strides=(2, 2))(last_conv)
 
     c3_pad = tf.keras.layers.ZeroPadding2D(((0, 1), (0, 1)))(c3)
     u1_c3 = Concatenate()([c3_pad, u1])
 
-    u2 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(u1_c3)
-    u2 = Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=(2, 2))(u2)
+    u2 = Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same')(u1_c3)
+    u2 = Conv2DTranspose(filters=128, kernel_size=(3, 3), strides=(2, 2))(u2)
     c2_pad = tf.keras.layers.ZeroPadding2D(((0, 3), (0, 3)))(c2)
     u2_c2 = Concatenate()([u2, c2_pad])
 
-    c4 = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(u2_c2)
-    u3 = Conv2DTranspose(filters=32, kernel_size=(3, 3), strides=(2, 2))(c4)
+    c4 = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(u2_c2)
+    u3 = Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=(2, 2))(c4)
 
     c1_pad = tf.keras.layers.ZeroPadding2D(((0, 7), (0, 7)))(c1)
     u3_c1 = Concatenate()([u3, c1_pad])
@@ -114,12 +112,11 @@ def fcnn_functional(image_shape, n_classes):
     c4 = tf.keras.layers.Cropping2D(cropping=((0, 7), (0, 7)))(c4)
 
     model = Model(inputs=x, outputs=c4) 
-    model.summary()
+    #model.summary()
     return model
 
 
 def fcnn_model(image_shape, n_classes):
-
     model = tf.keras.Sequential()
     # Must define the input shape in the first layer of the neural network
     model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=8, padding='same', activation='relu',
@@ -173,7 +170,7 @@ def train_model(image_directory, image_shape, box_size, epochs=3):
                      epochs=1, verbose=0)
     return model
 
-def evaluate_image(image_path, th=0.1):
+def evaluate_image(image_path, model, th=0.1):
     im = cv2.imread(image_path)
     im = normalize_image(im)
     in_img = np.zeros((1, 1080, 1920, 3))
@@ -204,7 +201,7 @@ if __name__ == '__main__':
     from sklearn.metrics import confusion_matrix
     path = '/home/thomas/bee-network/for_bees/Blank VS Scented/B VS S Day 1/Frames JPG/'
     shape = (1080, 1920, 3)
-    box_size = 3
+    box_size = 6 
     model_path = 'models/fcnn_functional.h5'
 
     if not os.path.isfile(model_path): 
@@ -218,11 +215,9 @@ if __name__ == '__main__':
     json = 'ground_truth_labels.json'
     class_mask, in_img = generate_class_mask(json, gt)
     class_mask[class_mask == -1] = 0
-    predictions = evaluate_image(gt)
-    num = len(class_mask[class_mask == predictions])
-    acc = num / (1080*1920)
+    predictions = evaluate_image(gt, model)
     iou = compute_iou(predictions, class_mask)
-    print("Acc (pixelwise): {}, IoU: {}".format(acc, iou))
+    print("IoU: {}".format(iou))
     fig, ax = plt.subplots(ncols=2, figsize=(12, 8))
     ax[0].imshow(predictions)
     ax[0].set_title("Preds, box_size={}, IoU={:.3f}".format(box_size, iou))
